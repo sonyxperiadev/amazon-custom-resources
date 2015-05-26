b4f9727e23ffa474f4f279be3d848b036af9b715
Anders Janmyr <anders.janmyr@sonymobile.com>
2015-05-26 13:09:50 +0200

    Added update functionaliry to create-role

--- create-role.sh
+++ create-role.sh
@@ -13,9 +13,31 @@ fi
 stack_name="$1"
 template="$2"
 
-aws cloudformation create-stack \
-  --region $region \
-  --stack-name $stack_name \
-  --template-body "`cat $template`" \
-  --capabilities CAPABILITY_IAM
+stack_exists() {
+  echo "Checking for stack $stack_name"
+  aws cloudformation describe-stacks --stack-name $stack_name > /dev/null 2>&1
+}
 
+create_stack() {
+  echo "CREATING stack $stack_name"
+  aws cloudformation create-stack \
+    --region $region \
+    --stack-name $stack_name \
+    --template-body "`cat $template`" \
+    --capabilities CAPABILITY_IAM
+}
+
+update_stack() {
+  echo "UPDATING stack $stack_name"
+  aws cloudformation update-stack \
+    --region $region \
+    --stack-name $stack_name \
+    --template-body "`cat $template`" \
+    --capabilities CAPABILITY_IAM
+}
+
+if stack_exists; then
+  update_stack
+else
+  create_stack
+fi
