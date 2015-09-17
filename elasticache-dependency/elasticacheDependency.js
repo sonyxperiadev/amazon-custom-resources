a1266da8a7470d4b79066e740d753d900b81e962
Anders Janmyr <anders.janmyr@sonymobile.com>
2015-09-17 11:04:25 +0200

    Consistent physical resource id for elasticacheDependency

--- elasticache-dependency/elasticacheDependency.js
+++ elasticache-dependency/elasticacheDependency.js
@@ -68,7 +68,7 @@ function sendResponse(event, context, status, data, err) {
     StackId: event.StackId,
     RequestId: event.RequestId,
     LogicalResourceId: event.LogicalResourceId,
-    PhysicalResourceId: context.logStreamName,
+    PhysicalResourceId: 'elasticacheDependency-' + event.ResourceProperties.CacheId,
     Status: status,
     Reason: getReason(err) + " See details in CloudWatch Log: " + context.logStreamName,
     Data: data
