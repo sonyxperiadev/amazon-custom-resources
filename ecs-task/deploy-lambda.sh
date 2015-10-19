135c18bbd1aaf3a202746dfb80c8cbb4b547edda
Anders Janmyr <anders.janmyr@sonymobile.com>
2015-10-19 14:42:39 +0200

    Deploy aws package since lambda uses old

--- ecs-task/deploy-lambda.sh
+++ ecs-task/deploy-lambda.sh
@@ -5,5 +5,9 @@
 func='ecsTask'
 role_stack_name='ecs-task-support'
 
+
+npm install --production
+zip -r ecsTask.zip ./ecsTask.js ./node_modules
+
 current_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-$current_dir/../deploy-lambda.sh $func $role_stack_name
+$current_dir/../deploy-lambda.sh $func $role_stack_name "$current_dir/ecsTask.zip"
